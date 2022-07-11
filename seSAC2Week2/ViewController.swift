628bd9d9552b3c271b0c544df093bda905f21e67
peisns <hansol1501@gmail.com>
2022-07-11 22:58:13 +0900

    Initial Commit

---
+++ seSAC2Week2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  seSAC2Week2
+//
+//  Created by Brother Model on 2022/07/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var greenView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
