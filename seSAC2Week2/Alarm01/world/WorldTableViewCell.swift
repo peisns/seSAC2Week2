5f4a4149c8bfce5a6ce3c5f6cada0c42087ce136
peisns <hansol1501@gmail.com>
2022-07-21 21:45:45 +0900

    making world time

---
+++ seSAC2Week2/Alarm01/world/WorldTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WorldTableViewCell.swift
+//  seSAC2Week2
+//
+//  Created by Brother Model on 2022/07/21.
+//
+
+import UIKit
+
+class WorldTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
